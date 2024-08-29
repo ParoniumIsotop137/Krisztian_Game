8aa397a933acb9bc15d66a8c0fc2bd587ca8e97d
ParoniumIsotop137 <horwathslos29@gmail.com>
2024-08-29 17:01:50 +0200

    v0.0.3
    Chef Munition: Werkzeug erstellt

---
+++ WrenchScript.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class WrenchScript : MonoBehaviour
+{
+    public float speed = 12f;
+    public Vector2 targetDirection;
+    public GameObject player;
+    public float rotationSpeed = 400f;
+    public Rigidbody2D wrenchRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("krisztian");
+        wrenchRb = GetComponent<Rigidbody2D>();
+
+        if (player != null)
+        {
+            targetDirection = (player.transform.position - transform.position).normalized;
+        }
+        wrenchRb.velocity = targetDirection * speed;
+        Destroy(gameObject, 5f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //transform.Translate(targetDirection * speed * Time.deltaTime);
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("krisztian"))
+        {
+            Debug.Log("Spieler getroffen!");
+
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.CompareTag("cnc_static") || collision.gameObject.CompareTag("vent_static"))
+        {
+
+            Destroy(gameObject);
+        }
+
+
+    }
+}
